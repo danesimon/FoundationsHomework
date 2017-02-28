060bfbf1199c9469cf7c99176e3208d236706109
danesimon <danesimonmobile@gmail.com>
2017-02-28 12:41:43 -0500

    Added html and CSS to organize and beautify the Quick Sneakers webpage
    Applied inline-block and floats to make the storefront more centered,
    used CSS to make the image scaling more appropriate and give the page a
    sleeker appearance.

---
+++ scripts.js
@@ -0,0 +1,30 @@
+
+
+var age = 25;
+
+if (age < 12) {
+  console.log("You're a child.");
+} else if (age === 12) {
+  console.log("You're exactly " + age + " years old!");
+} else if (age < 20) {
+  console.log("You're a teenager.");
+} else if (age === 20) {
+  console.log("You're exactly" + age + " years old!");
+} else if (age < 100)
+console.log ("You're probably an adult!");
+} else {
+  console.log("You're really old.! Or you didn't give me a number...");
+}
+
+
+
+
+
+var dog = {
+name    : "Bucky",
+color   : "black",
+age     : 14,
+isBlind : true,
+}
+
+console.log(dog);
